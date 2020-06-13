0348149ace5b2487d7c0594fa9ed559d411efdd8
Fikrimutaqin <32831882+Fikrimutaqin@users.noreply.github.com>
2020-06-13 16:47:32 +0700

    add
    add

---
+++ editArtikel.php
@@ -0,0 +1,51 @@
+<?php
+
+    include "koneksi.php";
+
+
+
+    $time = date("d M Y, H:i");
+
+    //$lead = str_replace("\r\n","<br>",$lead);
+
+    //$content = str_replace("\r\n","<br>",$content);
+
+    $lead = $_GET['lead'];
+
+    $content = $_GET['content];
+
+    $title = $_GET['title'];
+
+    $author = $_GET['author'];
+
+
+
+    $update="UPDATE articles SET judul='$title', penulis='$author', lead='$abstraksi', content='$content', waktu='$time' WHERE articleID ='$ID'";
+
+
+
+    $hasil=mysqli_query($connect,$update);
+
+
+
+    if ($hasil) {
+
+
+
+        echo "Artikel berhasil di update<br>";
+
+        echo "<a href=\"tampil_articles.php\">List</a>";
+
+        
+
+    } else {
+
+
+
+        echo "Artikel gagal di update";
+
+
+
+    }
+
+?> 
